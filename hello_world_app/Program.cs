e1c3075813633af87fd36eb18ba60caa22123948
alex <Kalinin-Alexey-Russia@yandex.ru>
2024-02-02 12:16:58 +0700

    imgui successfully included

--- hello_world_app/Program.cs
+++ hello_world_app/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ImGuiNET;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Initializing ImGui.NET...");
+
+        // Initialize ImGui.NET
+        ImGui.CreateContext();
+        var io = ImGui.GetIO();
+        io.Fonts.AddFontDefault();
+        
+        Console.WriteLine("ImGui.NET initialized. Running ImGui demo...");
+
+        // Main application loop
+        while (true)
+        {
+            // Start a new ImGui frame
+            ImGui.NewFrame();
+
+            // Sample ImGui code: create a simple window
+            ImGui.Begin("Hello, ImGui!");
+            ImGui.Text("This is a simple ImGui window.");
+            ImGui.End();
+
+            // End the ImGui frame
+            ImGui.Render();
+
+            // You would typically handle input and render ImGui here
+
+            // For simplicity, we'll just break the loop after one frame
+            break;
+        }
+
+        // Shutdown ImGui.NET
+        ImGui.DestroyContext();
+
+        Console.WriteLine("ImGui demo complete.");
+    }
+}
--- hello_world_app/obj/Debug/net8.0/hello_world_app.AssemblyInfo.cs
+++ hello_world_app/obj/Debug/net8.0/hello_world_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c018e2448f31c5a279a09407667c804acab2940c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b77cd5db69dafe27be76a54c9d5298221a2495")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
