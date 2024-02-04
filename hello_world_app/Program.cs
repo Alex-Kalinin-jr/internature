34a3f6bdebeced36ea6e513998c4dddc68a1f680
alex <kalinakrasnaya1990@gmail.com>
2024-02-04 18:52:47 +0300

    test_changes

--- hello_world_app/Program.cs
+++ hello_world_app/Program.cs
@@ -2,6 +2,7 @@
 {
     static void Main()
     {
+        // test changes
         System.Console.WriteLine("Initializing ImGui.NET...");
         System.Console.WriteLine("ImGui.NET initialized. Running ImGui demo...");
         System.Console.WriteLine("ImGui demo complete.");
