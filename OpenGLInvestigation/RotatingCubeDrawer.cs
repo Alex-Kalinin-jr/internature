fbbad94b83709e66ce24589940635b28b82a5230
alex <kalinakrasnaya1990@gmail.com>
2024-02-21 09:52:57 +0300

    check of faces correctness.

--- OpenGLInvestigation/RotatingCubeDrawer.cs
+++ OpenGLInvestigation/RotatingCubeDrawer.cs
@@ -81,15 +81,33 @@ public sealed class RotatingCubeDrawer {
     ++_lampCount;
     _lampPosition = lampBuff.PosVr;
 
-    Cube buff = new Cube(4, new Vector3(1.0f, 0.5f, 0.5f));
-    buff.PosVr += new Vector3(-3.0f, 0.0f, 1.0f);
+    Cube buff = new Cube(4, new Vector3(1.0f, 0.5f, 0.0f));
+    buff.PosVr += new Vector3(-3.0f, 0.0f, -1.0f);
+    buff.RotationVr += new Vector3(0.0f, 45.0f, 0.0f);
 
-    Cube buff2 = new Cube (10, new Vector3(1.0f, 1.0f, 0.0f));
+
+    Cube buff2 = new Cube (10, new Vector3(1.0f, 0.5f, 0.0f));
     buff2.PosVr += new Vector3(3.0f, 0.0f, -1.0f);
+    buff2.RotationVr += new Vector3(0.0f, 45.0f, 0.0f);
+
+    Cube buff3 = new Cube(10, new Vector3(1.0f, 0.5f, 0.0f));
+    buff3.PosVr += new Vector3(0.0f, 0.0f, -3.0f);
+    buff3.RotationVr += new Vector3(0.0f, 45.0f, 0.0f);
+
+    Cube buff4 = new Cube(10, new Vector3(1.0f, 0.5f, 0.0f));
+    buff4.PosVr += new Vector3(0.0f, 3.0f, -3.0f);
+    buff4.RotationVr += new Vector3(0.0f, 45.0f, 0.0f);
+
+    Cube buff5 = new Cube(10, new Vector3(1.0f, 0.5f, 0.0f));
+    buff5.PosVr += new Vector3(0.0f, -3.0f, -3.0f);
+    buff5.RotationVr += new Vector3(0.0f, 45.0f, 0.0f);
 
     _volumes.Add(lampBuff);
     _volumes.Add(buff);
     _volumes.Add(buff2);
+    _volumes.Add(buff3);
+    _volumes.Add(buff4);
+    _volumes.Add(buff5);
 
     _shader = new OpenGLInvestigation.Shader("Shader/Shaders/shader.vert", 
         "Shader/Shaders/shader.frag");
