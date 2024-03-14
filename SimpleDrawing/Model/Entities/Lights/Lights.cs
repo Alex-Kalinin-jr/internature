5ead71d4da5903ec0a0819326355ee1377071c54
alex <kalinakrasnaya1990@gmail.com>
2024-03-14 15:16:06 +0700

    some remarks

--- SimpleDrawing/Model/Entities/Lights/Lights.cs
+++ SimpleDrawing/Model/Entities/Lights/Lights.cs
@@ -8,7 +8,7 @@ namespace SimpleDrawing.Model {
     public Color ItsColor;
 
     public Light() {
-      ItsVolume = new Cube(4);
+      ItsVolume = new Cube(4); // just an example
       ItsColor = new Color();
 // as our scene consists of cubes with size 2x2x2, let us create light with different size.
       ItsVolume.ItsPosition.ScaleVr = new OpenTK.Mathematics.Vector3(0.1f, 0.1f, 0.1f);  // ScaleVr - good thing to do that.
