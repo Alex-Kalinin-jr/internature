e2ae9490f715b9dba4706e07ebe4a26740c2e8f4
alex <kalinakrasnaya1990@gmail.com>
2024-04-11 11:59:56 +0700

    structure of binary file tested

---
+++ DrawingEngine/Engine/Loader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+class Program {
+  static void Main() {
+    string filePath = "grid/grid.bin";
+    string filePath2 = "grid/grid.txt";
+
+    using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open))) {
+      int i = reader.ReadInt32();
+      int j = reader.ReadInt32();
+      int k = reader.ReadInt32();
+
+      float[] floats = new float[6 * i * j * k];
+      int index = 0;
+      for (int kk = 0; kk < k; ++kk) {
+        reader.ReadBoolean();
+
+        for (int ii = 0; ii < i; ++ii) {
+          for (int jj = 0; jj < j; ++jj) {
+            for (int corner = 0; corner < 4; ++corner) {
+              floats[index] = reader.ReadSingle();
+              ++index;
+            }
+          }
+        }
+      }
+
+      // Write the modified integers and floats back to the file
+      using (StreamWriter writer = new StreamWriter(File.Open(filePath2, FileMode.Create))) {
+        writer.Write(i);
+        writer.Write("\n");
+        writer.Write(j);
+        writer.Write("\n");
+        writer.Write(k);
+        writer.Write("\n");
+        writer.Write("\n");
+
+
+
+        foreach (var num in floats) {
+          writer.Write(num * (1 / 39.0f));
+          writer.Write("--");
+        }
+      }
+
+      Console.WriteLine("Integers and floats have been modified and written back to the file.");
+    }
+  }
+}
